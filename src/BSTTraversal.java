8d3e89922db50a0f9b942b4479a1782d458b2876
Dhruv <dhruvkalra@proton.me>
2024-09-30 19:25:57 -0400

    Representation of BST with implementation

--- src/BSTTraversal.java
+++ src/BSTTraversal.java
@@ -1,7 +1,59 @@
-public class BSTTraversal {
-    Node<>
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
-    public static void main(String[] args){
-        System.out.println("Hello I am here");
+public class BSTTraversal {
+
+    public void printTree(Node<Integer> root,int level){
+        if(root == null){
+            return;
+        }
+        for(int i =0;i<level;i++){
+            System.out.print("|");
+        }
+        System.out.println(root.data);
+        printTree(root.left,level+1);
+        printTree(root.right,level+1);
+
     }
+    public Node<Integer> treeByInOrder(int[] array){
+        Arrays.sort(array);
+        return createTree(array);
+    }
+    public Node<Integer> treeByPreOrder(int[] array){
+        Arrays.sort(array);
+        return createTree(array);
+    }
+    public Node<Integer> createTree(int[] array){
+        if(array.length>2) {
+            Node<Integer> node = new Node<>();
+            int rootIndex = array.length / 2;
+            node.data = array[rootIndex];
+            int[] passArrayLeft= new int[array.length/2];
+            int[] passArrayRight = new int[array.length - array.length/2 - 1];
+            System.arraycopy(array, 0, passArrayLeft, 0, rootIndex);
+            System.arraycopy(array, rootIndex+1,passArrayRight, 0, array.length-1-array.length/2);
+            node.left = createTree(passArrayLeft);
+            node.right=createTree(passArrayRight);
+            return node;
+        }else if(array.length == 2){
+            Node<Integer> node = new Node<>();
+            int rootIndex = array.length / 2;
+            node.data = array[rootIndex];
+            int[] passArrayLeft= new int[array.length/2];
+            System.arraycopy(array, 0, passArrayLeft, 0, rootIndex);
+            node.left = createTree(passArrayLeft);
+            node.right = null;
+            return node;
+        }else if(array.length == 1){
+            Node<Integer> node = new Node<>();
+            node.data = array[0];
+            node.left = null;
+            node.right =null;
+            return node;
+        }
+        return null;
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,15 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        BSTTraversal tree = new BSTTraversal();
+        int[] array = {15,10,20,8,12,17,25};
+        int[] preorder = {3, 9, 20, 15, 7};
+        int[] inorder={9, 3, 15, 20, 7};
+        tree.printTree(tree.treeByInOrder(inorder),0);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Node<Integer> root = tree.createTree(array);
+        tree.printTree(root,0);
+
+
     }
 }
